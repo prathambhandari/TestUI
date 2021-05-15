3d11d0d1071986eac061dad51cf9c8a0e6b05ee1
Prathambhandari <prathamkrishnabhandari17@gmail.com>
2021-05-15 16:37:53 +0530

    JustUI

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Test',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'Socially',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: GridView.count(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10.0,
+              mainAxisSpacing: 10.0,
+              shrinkWrap: true,
+              children: List.generate(
+                20,
+                (index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Container(color: Colors.cyan),
+                  );
+                },
+              )),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat_bubble_outline_rounded),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle_rounded),
+              label: '',
+            ),
+          ],
+          selectedItemColor: Colors.cyan,
+        ),
+      ),
+    );
+  }
+}
